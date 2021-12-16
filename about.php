fae009d0443d475b67b9b94b0017e7827ad4e80c
Andreas Caesareza <andreas.caesareza@erajaya.com>
2021-12-16 23:29:50 +0700

    php auto reload

---
+++ about.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>About Us</title>
+        <link rel="stylesheet" href="css/app.css" />
+        <script type="text/javascript" src="js/app.js"></script>
+    </head>
+    <body>
+        
+    <main class="container">
+    <ul class="menu">
+        <li>
+            <a href="index.php">Home</a>
+        </li>
+        <li>
+            <a href="about.php">About</a>
+        </li>
+    </ul>
+
+    <section class="content">
+    <?php
+        echo 'About';
+    ?>
+    <section>
+    </main>
+</html>
---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>Home Page</title>
+        <link rel="stylesheet" href="css/app.css" />
+        <script type="text/javascript" src="js/app.js"></script>
+    </head>
+    <body>
+    <main class="container">
+    <ul class="menu">
+        <li>
+            <a href="index.php">Home</a>
+        </li>
+        <li>
+            <a href="about.php">About</a>
+        </li>
+    </ul>
+
+    <section class="content">
+    <?php
+        echo 'Home';
+    ?>
+    <section>
+    </main>
+    <script id="__bs_script__">//<![CDATA[
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.27.7'><\/script>".replace("HOST", location.hostname));
+//]]></script>
+    </body>
+</html>
