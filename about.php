9e609a7453236f24d1bd7e2ff601b02a2cf5a3d8
Andreas Caesareza <andreas.caesareza@erajaya.com>
2021-12-23 07:29:07 +0700

    update about us page

--- about.php
+++ about.php
@@ -1,25 +1,69 @@
 <html>
-    <head>
-        <title>About Us</title>
-        <link rel="stylesheet" href="css/app.css" />
-        <script type="text/javascript" src="js/app.js"></script>
-    </head>
-    <body>
-        
-    <main class="container">
-    <ul class="menu">
-        <li>
-            <a href="index.php">Home</a>
-        </li>
-        <li>
-            <a href="about.php">About</a>
-        </li>
-    </ul>
+<head>
+    <title>About Us Page</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;700&display=swap" rel="stylesheet"/>
+    <link rel="stylesheet" href="css/app.css"/>
+    <script type="text/javascript" src="js/app.js"></script>
+</head>
+<body>
+<main class="container">
+    <header>
+        <div class="logo">@LisaManoban</div>
+        <ul class="menu">
+            <li>
+                <a href="index.php">Home</a>
+            </li>
+            <li>
+                <a href="about.php">About</a>
+            </li>
+        </ul>
+    </header>
 
-    <section class="content">
-    <?php
-        echo 'About';
-    ?>
-    <section>
-    </main>
+    <section id="content">
+        <aside>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut dolore, expedita itaque iure
+                maiores nobis nostrum optio quia quod saepe suscipit velit! Dolor dolore maiores obcaecati perferendis
+                praesentium quibusdam!</p>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut dolore, expedita itaque iure
+                maiores nobis nostrum optio quia quod saepe suscipit velit! Dolor dolore maiores obcaecati perferendis
+                praesentium quibusdam!</p>
+        </aside>
+        <section id="content-body">
+            <h1>About Us</h1>
+            <?php
+            echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut dolore, expedita itaque iure
+                maiores nobis nostrum optio quia quod saepe suscipit velit! Dolor dolore maiores obcaecati perferendis
+                praesentium quibusdam! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut dolore, expedita itaque iure
+                maiores nobis nostrum optio quia quod saepe suscipit velit! Dolor dolore maiores obcaecati perferendis
+                praesentium quibusdam!</p>';
+
+            echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut dolore, expedita itaque iure
+                maiores nobis nostrum optio quia quod saepe suscipit velit! Dolor dolore maiores obcaecati perferendis
+                praesentium quibusdam!</p>';
+
+            echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut dolore, expedita itaque iure
+                maiores nobis nostrum optio quia quod saepe suscipit velit! Dolor dolore maiores obcaecati perferendis
+                praesentium quibusdam! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut dolore, expedita itaque iure
+                maiores nobis nostrum optio quia quod saepe suscipit velit! Dolor dolore maiores obcaecati perferendis
+                praesentium quibusdam!</p>';
+
+            echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut dolore, expedita itaque iure
+                maiores nobis nostrum optio quia quod saepe suscipit velit! Dolor dolore maiores obcaecati perferendis
+                praesentium quibusdam!</p>';
+
+            echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut dolore, expedita itaque iure
+                maiores nobis nostrum optio quia quod saepe suscipit velit! Dolor dolore maiores obcaecati perferendis
+                praesentium quibusdam!</p>';
+            ?>
+        </section>
+    </section>
+
+    <footer>
+        &copy; 2021 RO YouTube Channel
+    </footer>
+</main>
+</body>
 </html>
