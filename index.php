a8034eb7de906ef73e81535118cbdc2eaa2c6e96
Andreas Caesareza <andreas.caesareza@erajaya.com>
2021-12-23 07:26:31 +0700

    browser sync test all device

--- index.php
+++ index.php
@@ -1,28 +1,71 @@
 <html>
-    <head>
-        <title>Home Page</title>
-        <link rel="stylesheet" href="css/app.css" />
-        <script type="text/javascript" src="js/app.js"></script>
-    </head>
-    <body>
-    <main class="container">
-    <ul class="menu">
-        <li>
-            <a href="index.php">Home</a>
-        </li>
-        <li>
-            <a href="about.php">About</a>
-        </li>
-    </ul>
+<head>
+    <title>Home Page</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;700&display=swap" rel="stylesheet"/>
+    <link rel="stylesheet" href="css/app.css"/>
+    <script type="text/javascript" src="js/app.js"></script>
+</head>
+<body>
+<main class="container">
+    <header>
+        <div class="logo">@LisaManoban</div>
+        <ul class="menu">
+            <li>
+                <a href="index.php">Home</a>
+            </li>
+            <li>
+                <a href="about.php">About</a>
+            </li>
+        </ul>
+    </header>
 
-    <section class="content">
-    <?php
-        echo 'Home';
-    ?>
-    <section>
-    </main>
-    <script id="__bs_script__">//<![CDATA[
+    <section id="content">
+        <aside>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut dolore, expedita itaque iure
+                maiores nobis nostrum optio quia quod saepe suscipit velit! Dolor dolore maiores obcaecati perferendis
+                praesentium quibusdam!</p>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut dolore, expedita itaque iure
+                maiores nobis nostrum optio quia quod saepe suscipit velit! Dolor dolore maiores obcaecati perferendis
+                praesentium quibusdam!</p>
+        </aside>
+        <section id="content-body">
+            <?php
+            echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut dolore, expedita itaque iure
+                maiores nobis nostrum optio quia quod saepe suscipit velit! Dolor dolore maiores obcaecati perferendis
+                praesentium quibusdam! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut dolore, expedita itaque iure
+                maiores nobis nostrum optio quia quod saepe suscipit velit! Dolor dolore maiores obcaecati perferendis
+                praesentium quibusdam!</p>';
+
+            echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut dolore, expedita itaque iure
+                maiores nobis nostrum optio quia quod saepe suscipit velit! Dolor dolore maiores obcaecati perferendis
+                praesentium quibusdam!</p>';
+
+            echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut dolore, expedita itaque iure
+                maiores nobis nostrum optio quia quod saepe suscipit velit! Dolor dolore maiores obcaecati perferendis
+                praesentium quibusdam! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut dolore, expedita itaque iure
+                maiores nobis nostrum optio quia quod saepe suscipit velit! Dolor dolore maiores obcaecati perferendis
+                praesentium quibusdam!</p>';
+
+            echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut dolore, expedita itaque iure
+                maiores nobis nostrum optio quia quod saepe suscipit velit! Dolor dolore maiores obcaecati perferendis
+                praesentium quibusdam!</p>';
+
+            echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut dolore, expedita itaque iure
+                maiores nobis nostrum optio quia quod saepe suscipit velit! Dolor dolore maiores obcaecati perferendis
+                praesentium quibusdam!</p>';
+            ?>
+        </section>
+    </section>
+
+    <footer>
+        &copy; 2021 RO YouTube Channel
+    </footer>
+</main>
+<script id="__bs_script__">//<![CDATA[
     document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.27.7'><\/script>".replace("HOST", location.hostname));
-//]]></script>
-    </body>
+    //]]></script>
+</body>
 </html>
